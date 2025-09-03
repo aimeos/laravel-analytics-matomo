d437a1dea29fd9a4b15d212694cf91448885142a
Aimeos <aimeos@aimeos.org>
2025-09-03 18:35:06 +0200

    Rewritten implementation to use visualappeal/matomo-php-api

--- src/Matomo.php
+++ src/Matomo.php
@@ -3,85 +3,112 @@
 namespace Aimeos\AnalyticsBridge\Drivers;
 
 use Aimeos\AnalyticsBridge\Drivers\Driver;
-use Matomo\API\Request;
+use VisualAppeal\Matomo as MatomoClient;
 
 
 class Matomo implements Driver
 {
-    protected string $url;
-    protected string $token;
-    protected int $siteId;
+    protected MatomoClient $client;
 
 
     public function __construct(array $config = [])
     {
-        $this->url = $config['url'] ?? '';
-        $this->token = $config['token'] ?? '';
-        $this->siteId = $config['siteid'] ?? 1;
+        $url = rtrim($config['url'] ?? '', '/');
+        $token = $config['token'] ?? '';
+        $siteId = (int) ($config['siteid'] ?? 1);
+
+        $this->client = new MatomoClient($url, $token, $siteId);
     }
 
 
     public function pageViews(string $path, int $days = 30): array
     {
-        $response = $this->query('Actions.getPageUrls', "pageUrl==$path", $days);
-        $data = [];
+        $response = $this->client->get('Actions.getPageUrls', [
+            'period' => 'day',
+            'date'   => "last{$days}",
+            'segment'=> "pageUrl==$path",
+        ]);
 
-        foreach ($response as $row) {
-            $data[$row['date']] = $row['nb_hits'] ?? 0;
-        }
-        return $data;
+        return $this->mapDaily($response, 'nb_hits');
     }
 
 
     public function visits(string $path, int $days = 30): array
     {
-        $response = $this->query('VisitsSummary.get', "pageUrl==$path", $days);
-        $data = [];
+        $response = $this->client->get('VisitsSummary.get', [
+            'period' => 'day',
+            'date'   => "last{$days}",
+            'segment'=> "pageUrl==$path",
+        ]);
 
-        foreach ($response as $row) {
-            $data[$row['date']] = $row['nb_visits'] ?? 0;
-        }
-        return $data;
+        return $this->mapDaily($response, 'nb_visits');
     }
 
 
     public function visitDurations(string $path, int $days = 30): array
     {
-        $response = $this->query('VisitsSummary.getAverageVisitDuration', "pageUrl==$path", $days);
-        $data = [];
+        $response = $this->client->get('VisitsSummary.get', [
+            'period' => 'day',
+            'date'   => "last{$days}",
+            'segment'=> "pageUrl==$path",
+        ]);
 
-        foreach ($response as $row) {
-            $data[$row['date']] = $row['avg_time_on_site'] ?? 0;
-        }
-        return $data;
+        return $this->mapDaily($response, 'avg_time_on_site');
     }
 
 
     public function countries(string $path, int $days = 30): array
     {
-        return $this->query('UserCountry.getCountry', "pageUrl==$path", $days);
+        $response = $this->client->get('UserCountry.getCountry', [
+            'period' => 'range',
+            'date'   => "last{$days}",
+            'segment'=> "pageUrl==$path",
+        ]);
+
+        return $this->mapAggregate($response, 'label', 'nb_visits');
     }
 
 
     public function referrers(string $path, int $days = 30): array
     {
-        return $this->query('Referrers.getWebsites', "pageUrl==$path", $days);
+        $response = $this->client->get('Referrers.getWebsites', [
+            'period' => 'range',
+            'date'   => "last{$days}",
+            'segment'=> "pageUrl==$path",
+        ]);
+
+        return $this->mapAggregate($response, 'label', 'nb_visits');
     }
 
 
-    protected function query(string $method, string $segment, int $days): array
+    /**
+     * Map daily responses into [ ['key'=>date, 'value'=>count], ... ]
+     */
+    protected function mapDaily(array $response, string $field): array
     {
-        $request = new Request([
-            'module' => 'API',
-            'method' => $method,
-            'idSite' => $this->siteId,
-            'period' => 'day',
-            'date' => "last{$days}",
-            'token_auth' => $this->token,
-            'segment' => $segment,
-            'format' => 'JSON',
-        ]);
+        $data = [];
+        foreach ($response as $date => $row) {
+            $data[] = [
+                'key' => $date,
+                'value' => $row[$field] ?? 0,
+            ];
+        }
+        return $data;
+    }
 
-        return $request->process();
+
+    /**
+     * Map aggregate responses into [ ['key'=>label, 'value'=>count], ... ]
+     */
+    protected function mapAggregate(array $response, string $labelField, string $valueField): array
+    {
+        $data = [];
+        foreach ($response as $row) {
+            $data[] = [
+                'key' => $row[$labelField] ?? '',
+                'value' => $row[$valueField] ?? 0,
+            ];
+        }
+        return $data;
     }
 }
