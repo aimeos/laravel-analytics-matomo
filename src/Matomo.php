406517dd1b588417a90f798992f7f2512bd8516f
Aimeos <aimeos@aimeos.org>
2025-09-05 16:41:48 +0200

    Rewritten using Laravel Http facade

--- src/Matomo.php
+++ src/Matomo.php
@@ -3,120 +3,178 @@
 namespace Aimeos\AnalyticsBridge\Drivers;
 
 use Aimeos\AnalyticsBridge\Contracts\Driver;
-use VisualAppeal\Matomo as MatomoClient;
+use Illuminate\Support\Facades\Http;
 
 
 class Matomo implements Driver
 {
-    private MatomoClient $client;
+    private ?string $siteId;
+    private ?string $token;
+    private ?string $url;
 
 
     public function __construct(array $config = [])
     {
-        $url = $config['url'] ?? '';
-        $token = $config['token'] ?? '';
-        $siteId = (int) ($config['siteid'] ?? 1);
-
-        $this->client = new MatomoClient($url, $token, $siteId, MatomoClient::FORMAT_JSON);
+        $this->siteId = $config['siteid'] ?? null;
+        $this->token = $config['token'] ?? null;
+        $this->url = $config['url'] ?? null;
     }
 
 
-    public function all(string $url, int $days = 30): array
+    public function all(string $url, int $days = 30): ?array
     {
-        // Methods to call
-        $methods = [
-            'Actions.getPageUrls',
-            'VisitsSummary.get',
-            'Referrers.getWebsites',
-            'UserCountry.getCountry',
+        if(!$this->url || !$this->token) {
+            return null;
+        }
+
+        $shared = [
+            'module'     => 'API',
+            'method'     => 'API.getBulkRequest',
+            'token_auth' => $this->token,
+            'idSite'     => $this->siteId,
+            'segment'    => "pageUrl==$url",
+            'date'       => "last{$days}",
+            'format'     => 'json',
+            'flat'       => 1,
         ];
 
-        // Shared parameters for all methods
-        $optional = [
-            'period'  => 'day',
-            'date'    => "last{$days}",
-            'segment' => "pageUrl==$url",
-            'flat'    => 1,
-        ];
+        $response = Http::get($this->url, $shared + [
+            'urls' => [
+                'method=Actions.getPageUrls&period=day',
+                'method=VisitsSummary.get&period=day',
+                'method=Referrers.getWebsites&period=range',
+                'method=UserCountry.getCountry&period=range'
+            ],
+        ]);
 
-        // Execute bulk request
-        $responses = $this->client->getBulkRequest($methods, $optional);
+        $data = [];
+
+        if(!$response->ok()) {
+            throw new Error( $response->getBody() );
+        }
+
+        $data = $response->json();
 
         return [
-            'pageViews'     => $this->mapDaily($responses[0], 'nb_hits'),
-            'visits'        => $this->mapDaily($responses[1], 'nb_visits'),
-            'visitDuration' => $this->mapDaily($responses[1], 'avg_time_on_site'),
-            'referrers'     => $this->mapAggregate($responses[2], 'label', 'url', 'nb_visits'),
-            'countries'     => $this->mapAggregate($responses[3], 'label', 'label', 'nb_visits'),
+            'views' => $this->mapDaily($response[0], 'nb_hits'),
+            'visits' => $this->mapDaily($response[1], 'nb_visits'),
+            'durations' => $this->mapDaily($response[1], 'avg_time_on_site'),
+            'referrers' => $this->mapAggregate($response[2], 'url', 'nb_visits'),
+            'countries' => $this->mapAggregate($response[3], 'label', 'nb_visits'),
         ];
     }
 
 
-    public function pageViews(string $url, int $days = 30): array
+    public function views(string $url, int $days = 30): ?array
     {
-        $segment = "pageUrl==$url";
-        $response = $this->client->request('Actions.getPageUrls', [
-            'period' => 'day',
-            'date'   => "last{$days}",
-            'segment'=> $segment,
+        if(!$this->url || !$this->token) {
+            return null;
+        }
+
+        $response = Http::get($this->url, [
+            'module'     => 'API',
+            'method'     => 'Actions.getPageUrls',
+            'idSite'     => $this->siteId,
+            'token_auth' => $this->token,
+            'segment'    => "pageUrl==$url",
+            'date'       => "last{$days}",
+            'format'     => 'json',
+            'period'     => 'day',
+            'flat'       => 1,
         ]);
 
-        return $this->mapDaily($response, 'nb_hits');
+        return $this->mapDaily($response->json(), 'nb_hits');
     }
 
 
-    public function visits(string $url, int $days = 30): array
+    public function visits(string $url, int $days = 30): ?array
     {
-        $segment = "pageUrl==$url";
-        $response = $this->client->getVisits($segment, [
-            'period' => 'day',
-            'date'   => "last{$days}",
+        if(!$this->url || !$this->token) {
+            return null;
+        }
+
+        $response = Http::get($this->url, [
+            'module'     => 'API',
+            'method'     => 'VisitsSummary.get',
+            'idSite'     => $this->siteId,
+            'token_auth' => $this->token,
+            'segment'    => "pageUrl==$url",
+            'date'       => "last{$days}",
+            'format'     => 'json',
+            'period'     => 'day',
         ]);
 
-        return $this->mapDaily($response, 'nb_visits');
+        return $this->mapDaily($response->json(), 'nb_visits');
     }
 
 
-    public function visitDurations(string $url, int $days = 30): array
+    public function durations(string $url, int $days = 30): ?array
     {
-        $segment = "pageUrl==$url";
-        $response = $this->client->getVisits($segment, [
-            'period' => 'day',
-            'date'   => "last{$days}",
+        if(!$this->url || !$this->token) {
+            return null;
+        }
+
+        $response = Http::get($this->url, [
+            'module'     => 'API',
+            'method'     => 'VisitsSummary.get',
+            'idSite'     => $this->siteId,
+            'token_auth' => $this->token,
+            'segment'    => "pageUrl==$url",
+            'date'       => "last{$days}",
+            'format'     => 'json',
+            'period'     => 'day',
         ]);
 
-        return $this->mapDaily($response, 'avg_time_on_site');
+        return $this->mapDaily($response->json(), 'avg_time_on_site');
     }
 
 
-    public function countries(string $url, int $days = 30): array
+    public function countries(string $url, int $days = 30): ?array
     {
-        $segment = "pageUrl==$url";
-        $response = $this->client->getCountries($segment, [
-            'period' => 'range',
-            'date'   => "last{$days}",
+        if(!$this->url || !$this->token) {
+            return null;
+        }
+
+        $response = Http::get($this->url, [
+            'module'     => 'API',
+            'method'     => 'UserCountry.getCountry',
+            'idSite'     => $this->siteId,
+            'token_auth' => $this->token,
+            'segment'    => "pageUrl==$url",
+            'date'       => "last{$days}",
+            'format'     => 'json',
+            'period'     => 'day',
         ]);
 
-        return $this->mapAggregate($response, 'label', 'nb_visits');
+        return $this->mapAggregate($response->json(), 'label', 'nb_visits');
     }
 
 
-    public function referrers(string $url, int $days = 30): array
+    public function referrers(string $url, int $days = 30): ?array
     {
-        $segment = "pageUrl==$url";
-        $response = $this->client->getWebsites($segment, [
-            'period' => 'range',
-            'date'   => "last{$days}",
+        if(!$this->url || !$this->token) {
+            return null;
+        }
+
+        $response = Http::get($this->url, [
+            'module'     => 'API',
+            'method'     => 'Referrers.getWebsites',
+            'idSite'     => $this->siteId,
+            'token_auth' => $this->token,
+            'segment'    => "pageUrl==$url",
+            'date'       => "last{$days}",
+            'format'     => 'json',
+            'period'     => 'day',
         ]);
 
-        return $this->mapAggregate($response, 'label', 'nb_visits');
+        return $this->mapAggregate($response->json(), 'label', 'nb_visits');
     }
 
 
     /**
      * Map daily responses into [ ['key'=>date, 'value'=>count], ... ]
      */
-    protected function mapDaily(object $response, string $field): array
+    protected function mapDaily(array $response, string $field): array
     {
         return collect($response)
             ->map(fn($items, $date) => [
@@ -131,16 +189,13 @@ class Matomo implements Driver
     /**
      * Map aggregate responses into [ ['key'=>label, 'value'=>count], ... ]
      */
-    protected function mapAggregate(object $response, string $groupField, string $labelField, string $valueField): array
+    protected function mapAggregate(array $response, string $labelField, string $valueField): array
     {
         return collect($response)
-            ->flatten(1) // flatten one level: remove dates
-            ->groupBy($groupField)
-            ->map(fn($group) => [
-                'key' => $group->first()?->$labelField ?? $group->first()?->$groupField,
-                'value' => $group->sum($valueField),
+            ->map(fn($item) => [
+                'key' => $item[$labelField],
+                'value' => $item[$valueField],
             ])
-            ->values() // reset numeric keys
             ->all();
     }
 }
