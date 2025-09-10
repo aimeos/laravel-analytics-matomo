14d450d16ac3e9a0790ee9ec93abc2b74f7638be
Aimeos <aimeos@aimeos.org>
2025-09-11 00:42:34 +0200

    Added types() method

--- src/Matomo.php
+++ src/Matomo.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Http;
 
 class Matomo implements Driver
 {
+    private array $types = ['views', 'visits', 'durations', 'conversions', 'countries', 'referrers'];
     private ?string $siteId;
     private ?string $token;
     private ?string $url;
@@ -21,7 +22,7 @@ class Matomo implements Driver
     }
 
 
-    public function stats(string $url, int $days = 30, array $types = []): ?array
+    public function stats(string $url, int $days = 30): ?array
     {
         if(!$this->url || !$this->token) {
             return null;
@@ -37,26 +38,25 @@ class Matomo implements Driver
             'format'     => 'json',
         ];
 
-        $types = empty($types) ? ['views', 'visits', 'durations', 'conversions', 'countries', 'referrers'] : $types;
         $urls = [];
 
-        if(in_array('views', $types)) {
+        if(in_array('views', $this->types)) {
             $urls[] = 'method=Actions.getPageUrls&period=day';
         }
 
-        if(in_array('visits', $types) || in_array('durations', $types)) {
+        if(in_array('visits', $this->types) || in_array('durations', $this->types)) {
             $urls[] = 'method=VisitsSummary.get&period=day&flat=1';
         }
 
-        if(in_array('conversions', $types)) {
+        if(in_array('conversions', $this->types)) {
             $urls[] = 'method=Goals.get&period=day';
         }
 
-        if(in_array('countries', $types)) {
+        if(in_array('countries', $this->types)) {
             $urls[] = 'method=UserCountry.getCountry&period=range';
         }
 
-        if(in_array('referrers', $types)) {
+        if(in_array('referrers', $this->types)) {
             $urls[] = 'method=Referrers.getReferrerType&expanded=1&period=range';
         }
 
@@ -69,35 +69,42 @@ class Matomo implements Driver
         $data = $response->json();
         $result = [];
 
-        if(in_array('views', $types)) {
+        if(in_array('views', $this->types)) {
             $result['views'] = $this->mapDaily(array_shift($data) ?? [], 'nb_hits');
         }
 
-        if(in_array('visits', $types) || in_array('durations', $types)) {
+        if(in_array('visits', $this->types) || in_array('durations', $this->types)) {
             $entries = array_shift($data) ?? [];
 
-            if(in_array('visits', $types)) {
+            if(in_array('visits', $this->types)) {
                 $result['visits'] = $this->mapDaily($entries, 'nb_visits');
             }
 
-            if(in_array('durations', $types)) {
+            if(in_array('durations', $this->types)) {
                 $result['durations'] = $this->mapDaily($entries, 'avg_time_on_site');
             }
         }
 
-        if(in_array('conversions', $types)) {
+        if(in_array('conversions', $this->types)) {
             $result['conversions'] = $this->mapDaily(array_shift($data) ?? [], 'nb_conversions');
         }
 
-        if(in_array('countries', $types)) {
+        if(in_array('countries', $this->types)) {
             $result['countries'] = $this->mapAggregate(array_shift($data) ?? []);
         }
 
-        if(in_array('referrers', $types)) {
+        if(in_array('referrers', $this->types)) {
             $result['referrers'] = $this->mapReferrers(array_shift($data) ?? []);
         }
 
         return $result;
+    }
+
+
+    public function types(array $types): Driver
+    {
+        $this->types = $types;
+        return $this;
     }
 
 
